6298c5eec5703a855aa10980a7252806b90968f4
a <ruan65@gmail.com>
2019-06-09 21:13:11 +0300

    remove counter

---
+++ ios/DerivedDataBuild/Runner/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/a/dev/grpc/tensor/flutter_chat_grpc_client/ios/DerivedDataBuild/Runner/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
